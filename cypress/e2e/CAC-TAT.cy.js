79a4dea6a815c87b3ece5d74a47b2f70f212bcbe
Luiz Barros <lck.barros@gmail.com>
2025-02-21 23:08:11 -0300

    Minha resolução do exercicio 2 de automatização de preenchimento de formulário, realização de clique e verificação de surgimento de conteúdo

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,7 +1,18 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-
-  it('verifica o título da aplicação', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
+
+  it('verifica o título da aplicação', () => {    
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Ambrose')
+    cy.get('#email').type('john@ambrose.io')
+    cy.get('#open-text-area').type('Testando minúsculo e MAIÚSCULO, assim como assentuação.')    
+    cy.get('.button').click()
+    cy.get('.success').should('be.visible')
   })
 })
