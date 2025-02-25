0080a87c3bb4fec23e1c41073c91fe4dfc7be4a4
Luiz Barros <lck.barros@gmail.com>
2025-02-24 21:22:39 -0300

    Aula 02, exercício extra 1 e 2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -11,8 +11,18 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('#firstName').type('João')
     cy.get('#lastName').type('Ambrose')
     cy.get('#email').type('john@ambrose.io')
-    cy.get('#open-text-area').type('Testando minúsculo e MAIÚSCULO, assim como assentuação.')    
+    cy.get('#open-text-area').type('Testando minúsculo e MAIÚSCULO, assim como assentuação.', {delay: 0})    
     cy.get('.button').click()
     cy.get('.success').should('be.visible')
+  })
+
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Ambrose')
+    cy.get('#email').type('john@ambrose,io')
+    cy.get('#open-text-area').type('Testando minúsculo e MAIÚSCULO, assim como assentuação.', {delay: 0})    
+    cy.get('.button').click()
+
+    cy.get('.error').should('be.visible')
   })
 })
