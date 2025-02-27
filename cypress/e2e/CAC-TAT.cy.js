330e5ad3a5431d4a1eedb9eb9fa8f6b9a50cd675
Luiz Barros <lck.barros@gmail.com>
2025-02-26 23:14:36 -0300

    Minha resolução do exercício extra 4 da aula 2

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -26,12 +26,23 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get('.error').should('be.visible')
   })
 
-  it('teste de valor nao-numerico digitado', () => {
+  it('teste de valor nao-numerico digitado e permanecendo vazio', () => {
     cy.get('#firstName').type('João')
     cy.get('#lastName').type('Ambrose')
     cy.get('#email').type('john@ambrose,io')
     cy.get('#open-text-area').type('Testando minúsculo e MAIÚSCULO, assim como assentuação.', {delay: 0})    
     cy.get('#phone').type('abc').should('have.value', '')
     cy.get('.button').click()
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Ambrose')
+    cy.get('#email').type('john@ambrose,io')
+    cy.get('#open-text-area').type('Testando minúsculo e MAIÚSCULO, assim como assentuação.', {delay: 0})
+    cy.get('#phone-checkbox').click()
+    cy.get('.button').click()
+
+    cy.get('.error').should('be.visible')
   })
 })
